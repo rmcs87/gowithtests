e5002ac50e4a33d5ad7798cf625fd8f8431bd5d2
Ricardo Mendes <rmcs87@gmail.com>
2022-04-07 16:57:25 -0300

    Language Support

--- cmd/01_hello/hello.go
+++ cmd/01_hello/hello.go
@@ -4,13 +4,16 @@ import "fmt"
 
 const englishHelloPrefix = "Hello, "
 
-func Hello(name string) string {
+func Hello(name string, language string) string {
 	if name == "" {
 		name = "World"
+	}
+	if language == "Spanish" {
+		return "Holla, " + name
 	}
 	return englishHelloPrefix + name
 }
 
 func main() {
-	fmt.Println(Hello("World"))
+	fmt.Println(Hello("World", ""))
 }
