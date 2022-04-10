008d0adc521765c0cf358b61842d4eccd1aecb25
Ricardo Mendes <rmcs87@gmail.com>
2022-04-10 18:52:45 -0300

    generics

---
+++ cmd/13-generics/generics_test.go
@@ -0,0 +1,70 @@
+package generics
+
+import "testing"
+
+func TestStack(t *testing.T) {
+	t.Run("integer stack", func(t *testing.T) {
+		myStackOfInts := new(Stack[int])
+
+		// check stack is empty
+		AssertTrue(t, myStackOfInts.IsEmpty())
+
+		// add a thing, then check it's not empty
+		myStackOfInts.Push(123)
+		AssertFalse(t, myStackOfInts.IsEmpty())
+
+		// add another thing, pop it back again
+		myStackOfInts.Push(456)
+		value, _ := myStackOfInts.Pop()
+		AssertEqual(t, value, 456)
+		value, _ = myStackOfInts.Pop()
+		AssertEqual(t, value, 123)
+		AssertTrue(t, myStackOfInts.IsEmpty())
+
+		// can get the numbers we put in as numbers, not untyped interface{}
+		myStackOfInts.Push(1)
+		myStackOfInts.Push(2)
+		firstNum, _ := myStackOfInts.Pop()
+		secondNum, _ := myStackOfInts.Pop()
+		AssertEqual(t, firstNum+secondNum, 3)
+	})
+}
+
+func TestAssertFuntions(t *testing.T) {
+	t.Run("asserting on integers", func(t *testing.T) {
+		AssertEqual(t, 1, 1)
+		AssertNotEqual(t, 1, 2)
+	})
+	t.Run("assert on strings", func(t *testing.T) {
+		AssertEqual(t, "hello", "hello")
+		AssertNotEqual(t, "hello", "Ricardo")
+	})
+}
+
+func AssertNotEqual[T comparable](t *testing.T, got, want T) {
+	t.Helper()
+	if got == want {
+		t.Errorf("got %v, want %v", got, want)
+	}
+}
+
+func AssertEqual[T comparable](t *testing.T, got, want T) {
+	t.Helper()
+	if got != want {
+		t.Errorf("got %v, want %v", got, want)
+	}
+}
+
+func AssertTrue(t *testing.T, got bool) {
+	t.Helper()
+	if !got {
+		t.Errorf("got %v, want true", got)
+	}
+}
+
+func AssertFalse(t *testing.T, got bool) {
+	t.Helper()
+	if got {
+		t.Errorf("got %v, want false", got)
+	}
+}
